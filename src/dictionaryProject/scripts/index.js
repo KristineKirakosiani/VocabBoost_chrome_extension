5a3aab662700f351c894291e6e6aa8da733cd53a
Kristine Kirakosiani <kristine.kirakossiani@gmail.com>
2023-01-21 14:45:00 +0100

    fix missing require, use parcel to bundle the javascript into a single file and eliminate require

--- src/dictionaryProject/scripts/index.js
+++ src/dictionaryProject/scripts/index.js
@@ -1,16 +1,15 @@
+
 function pop_a_word(word, language) {
-    var wd = require(["word-definition"]);
-  
-    wd.getDef(word, language, null, function (definition) {
-      console.log(definition);
-  
-    });
-  }
+  let wd = require("word-definition");
+  wd.getDef(word, language, null, function (definition) {
+    console.log(definition);
+  });
+}
 
 function get_input_word() {
-  let btn = document.querySelector("#btn");
-  btn.addEventListener("click", function () {
-    input = document.querySelector("#input").value;
+  let myButton = document.querySelector("#btn");
+  myButton.addEventListener("click", function () {
+    let input = document.querySelector("#input").value;
     console.log(input)
     pop_a_word(input,"fr")
   });

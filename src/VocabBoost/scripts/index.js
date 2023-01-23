a0f12034b9e10d6121cba5daf30342a85303a220
Kristine Kirakosiani <kristine.kirakossiani@gmail.com>
2023-01-23 21:57:29 +0100

    fixed

--- src/VocabBoost/scripts/index.js
+++ src/VocabBoost/scripts/index.js
@@ -1,17 +1,24 @@
+let myButton = document.querySelector("#btn")
+let languageList = document.querySelector("#languageList")
+let outputBox = document.querySelector("#output")
+
+
 
 function pop_a_word(word, language) {
   let wd = require("word-definition");
   wd.getDef(word, language, null, function (definition) {
     console.log(definition);
+    outputBox.innerHTML = definition.definition
   });
 }
 
 function get_input_word() {
-  let myButton = document.querySelector("#btn");
   myButton.addEventListener("click", function () {
     let input = document.querySelector("#input").value;
+    let chosenLanguage = document.querySelector("#chosenLanguage").value;
+    console.log(chosenLanguage)
     console.log(input)
-    pop_a_word(input,"fr")
+    pop_a_word(input, chosenLanguage)
   });
 }
 get_input_word();

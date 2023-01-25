477c2756b4041aa8eb2c6845ea1e0821e821c0b8
Klervy <112544312+KatShulu@users.noreply.github.com>
2023-01-25 10:09:27 +0100

    rework files emplacement

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,51 @@
-let myButton = document.querySelector("#btn_translate")
-let outputBox = document.querySelector("#pop_definition")
-let firstPage = document.getElementById("firstPage")
-let random_word_page = document.getElementById("random_word_page")
-let btn_switch = document.getElementById("btn_switch")
-
+let myButton = document.querySelector("#btn_translate");
+let outputBox = document.querySelector("#pop_definition");
+let firstPage = document.getElementById("firstPage");
+let random_word_page = document.getElementById("random_word_page");
+let btn_switch = document.getElementById("btn_switch");
 
 function pop_a_word(word, language) {
-  
   let wd = require("word-definition");
   wd.getDef(word, language, null, function (definition) {
-    //console.log(definition.definition);
-    outputBox.innerHTML = definition.definition
-      });
-
+    outputBox.innerHTML = definition.definition;
+  });
 }
 
 function get_input_word() {
   myButton.addEventListener("click", function () {
     let input = document.querySelector("#input_word").value;
     let chosenLanguage = document.querySelector("#chosenLanguage").value;
-    console.log(chosenLanguage)
-    console.log(input)
-    pop_a_word(input, chosenLanguage)
+    pop_a_word(input, chosenLanguage);
   });
 }
 get_input_word();
 
-
-let array_word=["time","person","year","way","day","thing","man","world","life","hand","part","child","eye","woman","place","work","week","case","point","government","company" ,"number","group","problem","fact"]
+let array_word = [
+  "time",
+  "person",
+  "year",
+  "way",
+  "day",
+  "thing",
+  "man",
+  "world",
+  "hand",
+  "part",
+  "child",
+  "eye",
+  "woman",
+  "place",
+  "work",
+  "week",
+  "case",
+  "point",
+  "government",
+  "company",
+  "number",
+  "group",
+  "problem",
+  "fact",
+];
 /*
 function show_random_word(){
     setInterval(function (){
@@ -42,29 +60,29 @@ function show_random_word(){
     }, 6000);
 
 }*/
-function show_random_word(){
+function show_random_word() {
   document.querySelector("#btn_random").addEventListener("click", function () {
-    var randomElement = array_word[Math.floor(Math.random() * array_word.length)];
-    document.getElementById("random_word").innerHTML = randomElement
-    
+    var randomElement =
+      array_word[Math.floor(Math.random() * array_word.length)];
+    document.getElementById("random_word").innerHTML = randomElement;
+
     let wd = require("word-definition");
     wd.getDef(randomElement, "en", null, function (definition) {
       console.log(definition.definition);
-      document.getElementById("random_def").innerHTML = definition.definition
+      document.getElementById("random_def").innerHTML = definition.definition;
     });
   });
-
 }
-
-function switch_between_pages(){
+show_random_word();
+function switch_between_pages() {
   btn_switch.addEventListener("click", function () {
-    if(getComputedStyle(firstPage).display != "none"){
+    if (getComputedStyle(firstPage).display != "none") {
       firstPage.style.display = "none";
-      random_word_page.style.display = "block";
+      random_word_page.style.display = "flex";
     } else {
-      firstPage.style.display = "block";
+      firstPage.style.display = "flex";
       random_word_page.style.display = "none";
     }
   });
 }
-switch_between_pages()
+switch_between_pages();
